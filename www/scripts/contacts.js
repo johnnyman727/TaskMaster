c0a47a53f4212d2b0404d5a2547b44f2856d1e25
Patrick Varin <Patrick.Varin@students.olin.edu>
2012-10-29 20:56:57 -0400

    Add Friends page finished, still need to add the selected contacts to the master list and remove the selected contacts from the phone contacts list.     Next: create the add group page (this hasnt really been designed yet, we will see how it goes)

--- www/scripts/contacts.js
+++ www/scripts/contacts.js
@@ -1,3 +1,17 @@
+/* contacts.js: for use with the AddContacts page
+ * 
+ * Defines Contact and ContactList to store contacts and manipulate
+ * lists of contacts
+ * 
+ * Populates the phone contacts list with default contacts
+ * Encodes functionality for adding and removing contacts from the
+ * friends list
+ * 
+ * 
+ */
+ 
+/* Contact and ContactList constructors */
+
 function Contact(name, imgPath){
 	this.name = name;
 	this.id = name.replace(/\s/g, "");
@@ -40,10 +54,77 @@ function ContactList(){
 	}
 }
 
+/* On click friend item handler */
+function toggleContactState(event){
+	var id = event.data.id
+	friendElement = $('#'+id)
+	if (addedContacts.hasContact(id)){
+		//remove the contact
+		addedContacts.removeContact(id);
+		//change the style
+		friendElement.removeClass('ui-btn-hover-f');
+		friendElement.removeClass('ui-btn-up-f');
+		friendElement.addClass('ui-btn-up-c');
+		friendElement.removeClass('ui-btn-hover-c');
+		friendElement.attr('data-theme','c');
+	}else{
+		//add the contact
+		addedContacts.addContact(phoneContacts.getContact(id));
+		//change the style
+		friendElement.removeClass('ui-btn-hover-c');
+		friendElement.removeClass('ui-btn-up-c');
+		friendElement.addClass('ui-btn-hover-c');
+		friendElement.addClass('ui-btn-up-f');
+		
+		friendElement.attr('data-theme','f');
+	}
+}
+
+/* Populate the HTML list */
+function updateAddFriendsHTML(cList){
+	var ulFriendsNode = $("#addFriendsList");
+	var liExample = $("#addFriendsList > li:first").clone()
+	ulFriendsNode.empty();
+	
+	for (var i=0; i<cList.contacts.length; i++){
+		var friendLI = liExample.clone();
+		//Make the theme consistant
+		friendLI.removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-up-f');
+		friendLI.addClass('ui-btn-up-c');
+		friendLI.attr('data-theme','c');
+		
+		//Add button functionality
+		var friendButton = friendLI.find('a:first');
+		
+		//Set the handler
+		friendButton.click({id:cList.contacts[i].id},toggleContactState);
+		
+		//Change the image and the name
+		friendLI.find('img:first').attr('src',cList.contacts[i].imgPath);
+		friendLI.find('h1:first').html(cList.contacts[i].name);
+		
+		//Assign the li an id
+		friendLI.attr('id',cList.contacts[i].id);
+		
+		//Add friend node to the ul
+		ulFriendsNode.append(friendLI);
+	}
+}
+
+/*Continue Button Handler*/
+function continueEvent(event){
+	//FIXME: add people to the master list
+}
+
+function updateContinueButton(){
+	continueButton = $('#continueButton')
+	continueButton.click(continueEvent)
+}
+
+/*Initialize the contact lists*/
 phoneContacts = new ContactList();
 addedContacts = new ContactList();
 
-/*Initialize the contact lists*/
 var contactNames = [];
 contactNames[0] = "Danny Marz";
 contactNames[1] = "Jasper Maniates-Selvin";
@@ -72,70 +153,7 @@ for (var i=0; i < contactNames.length; i++){
 
 phoneContacts.sort();
 
-function toggleContactState(id){
-	friendElement = $('#'+id)
-	if (addedContacts.hasContact(id)){
-		//alert('removing contact'+id)
-		//remove the contact
-		addedContacts.removeContact(id);
-		//change the style
-		friendElement.removeClass('ui-btn-hover-f');
-		friendElement.removeClass('ui-btn-up-f');
-		friendElement.addClass('ui-btn-up-c');
-		friendElement.removeClass('ui-btn-hover-c');
-		friendElement.attr('data-theme','c');
-	}else{
-		//alert('adding contact'+id)
-		//add the contact
-		addedContacts.addContact(phoneContacts.getContact(id));
-		//change the style
-		friendElement.removeClass('ui-btn-hover-c');
-		friendElement.removeClass('ui-btn-up-c');
-		friendElement.addClass('ui-btn-hover-c');
-		friendElement.addClass('ui-btn-up-f');
-		
-		friendElement.attr('data-theme','f');
-	}
-	//alert(addedContacts.contacts.length+' friends')
-	/*var currentFriends = ''
-	/for (var i=0;i<addedContacts.length;i++){
-		currentFriends = currentFriends + addedContacts.contacts[i].name + '\n'
-	}
-	
-	alert('Current Friends:\n'+currentFriends)*/
-}
+$(document).ready(function () {updateAddFriendsHTML(phoneContacts); updateContinueButton();});
 
-function updateAddFriendsHTML(cList){
-	var ulFriendsNode = $("#addFriendsList");
-	var liExample = $("#addFriendsList > li:first").clone()
-	ulFriendsNode.empty();
-	
-	for (var i=0; i<cList.contacts.length; i++){
-		var friendLI = liExample.clone();
-		//Make the theme consistant
-		friendLI.removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-up-f');
-		friendLI.addClass('ui-btn-up-c');
-		friendLI.attr('data-theme','c');
-		
-		//Add button functionality
-		var friendButton = friendLI.find('a:first');
-		
-		//Set the onClick handler
-		function onClick(event){
-			toggleContactState(event.data.contact.id);
-		}
-		friendButton.click({contact:cList.contacts[i]},onClick);
-		
-		//Change the image and the name
-		friendLI.find('img:first').attr('src',cList.contacts[i].imgPath);
-		friendLI.find('h1:first').html(cList.contacts[i].name);
-		
-		//Assign the li an id
-		friendLI.attr('id',cList.contacts[i].id);
-		
-		//Add friend node to the ul
-		ulFriendsNode.append(friendLI);
-	}
-}
-
-$(document).ready(function () {updateAddFriendsHTML(phoneContacts)});
+//$.cookie('phoneContacts',phoneContacts)
+//$.cookie('addedContacts',addedContacts)
