faaac3edb70110b846384aa939f2966df03ce967
Jon McKay <jonathan.mckay@students.olin.edu>
2012-10-30 22:09:03 -0400

    Finishing sidebar + misc additions

--- www/scripts/Task.js
+++ www/scripts/Task.js
@@ -1,5 +1,10 @@
 Task = function(title, creator) {
 	this.title = title;
 	this.creator = creator;
-
+	this.lat = null;
+	this.lng = null;
+	this.location = null;
+	this.resolvedBy = null;
+	this.active = true;
+	this.price = null;
 }
