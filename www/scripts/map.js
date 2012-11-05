88f218da3b2a983aaf28d9b9209c3d76846976fa
Rachel Sirkin <rachel.sirkin@students.olin.edu>
2012-11-04 21:05:49 -0500

    Now with a working map!

--- www/scripts/map.js
+++ www/scripts/map.js
@@ -1,14 +1,18 @@
 var map = null;
 function initialize() {
-	alert('initializing map')
-	var mapOptions = {
-		center: new google.maps.LatLng(42.292612, -71.263247),
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map($("#map_canvas"),
-	mapOptions);
+    
 }
+
+
+$('#home-map').live("pageshow", function() {
+		$('#map_canvas').gmap('refresh');
+});
+$('#home-map').live("pageinit", function() {
+		$('#map_canvas').gmap({'center': '42.2935, -71.2639'});
+		$('#map_canvas').gmap('option', 'zoom', 15);
+		$('#map_canvas').gmap('addMarker', {'id': 'm1', 'position': '42.2935, -71.2639', 'bounds':false});
+});
+
 	
 $(document).bind('pageinit',initialize);
 $('#map_canvas').bind('click',initialize);
