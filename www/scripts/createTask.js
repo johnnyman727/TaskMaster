108ed96c3e6cdf8b849ceffeee3098387073f5e7
Patrick Varin <Patrick.Varin@students.olin.edu>
2012-11-05 01:46:18 -0500

    committing for the night

---
+++ www/scripts/createTask.js
@@ -0,0 +1,46 @@
+function createTaskFromForm(){
+	//title
+	var newTask = new Task($('#addTask-title').val(),'You');
+	$('#addTask-title').val('');
+	
+	//shared with
+	if ($('#addTask-sharedWith').val()!=''){
+		newTask.setSharedWith($('#addTask-sharedWith').val());//FIXME make these references to contacts
+		$('#addTask-sharedWith').val('');
+	}
+	
+	//location
+	if ($('#addTask-location').val()!=''){
+		newTask.setLocation($('#addTask-location',0,0).val());//FIXME latitude and longitude
+		$('#addTask-location').val('')
+	}
+	
+	//brand
+	if ($('#addTask-brand').val()!=''){
+		newTask.setBrand($('#addTask-brand').val());//FIXME make these references to contacts
+		$('#addTask-brand').val('');
+	}
+	
+	//maxPrice
+	if ($('#addTask-price').val()!=''){
+		newTask.setMaxPrice($('#addTask-price').val());//FIXME make these references to contacts
+		$('#addTask-price').val('');
+	}
+	
+	//FIXME deal with the priority
+	$('#addTask-priority-high')
+	$('#addTask-priority-medium')
+	$('#addTask-priority-low')
+	
+	//notes
+	$('#addTask-notes')
+	if ($('#addTask-notes').val()!=''){
+		newTask.setNotes($('#addTask-notes').val());//FIXME make these references to contacts
+		$('#addTask-notes').val('');
+	}
+	
+	me.taskList.addTask(newTask);
+	console.log(me.taskList);
+	updateContentHTML();
+}
+$(document).ready(function(){$('#addTask-add').click(createTaskFromForm);})
